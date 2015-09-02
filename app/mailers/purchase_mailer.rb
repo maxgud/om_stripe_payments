d93e8babdff82989888ed7422b1cd0db8930b11a
maxgud <maxgud@gmail.com>
2015-09-02 16:45:16 -0600

    stupid mistake

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,6 +1,6 @@
 class PurchaseMailer < ActionMailer::Base
 	layout 'purchase_mailer'
-	default from: "Matt Smitt Payments <mattsmittt@gmail.com"
+	default from: "Matt Smitt Payments <mattsmittt@gmail.com>"
 
 	def purchase_receipt purchase
 		@purchase = purchase
