0614e109d394c8e5b9a9e1c85413f4e825b5757b
maxgud <maxgud@gmail.com>
2015-09-02 19:06:54 -0600

    email issues

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,9 +1,9 @@
 class PurchaseMailer < ActionMailer::Base
 	layout 'purchase_mailer'
-	default from: "Matt Smitt Payments <mattsmittt@gmail.com>"
+	default from: "One Month Commerce <commerce@onemonth.com>"
 
 	def purchase_receipt purchase
 		@purchase = purchase
-		mail to: purchase.email, subject: "Thanks for your purchase"
+		mail to: purchase.email, subject: "Thanks for your purchase!"
 	end
 end
