b9b864ca2748a1b308f2ac5fde78e91ed7f1c401
maxgud <maxgud@gmail.com>
2015-09-02 22:49:41 -0600

    sendgrid restart

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,25 +2,25 @@ class ChargesController < ApplicationController
 
 		def create
 
-	  customer = Stripe::Customer.create(
-	    :email => params[:stripeEmail],
-	    :card  => params[:stripeToken]
-	  )
+      customer = Stripe::Customer.create(
+        :email => params[:stripeEmail],
+        :card  => params[:stripeToken]
+      )
 
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => params[:amount],
-	    :description => 'Growth Hacking Crash Course',
-	    :currency    => 'usd'
-	  )
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => params[:amount],
+        :description => 'Growth Hacking Crash Course',
+        :currency    => 'usd'
+      )
 
-	  	purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], amount: params[:amount], description: charge.description, currency: charge.currency, customer_id: customer.id, product_id: 1, uuid: SecureRandom.uuid)
+        purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], amount: params[:amount], description: charge.description, currency: charge.currency, customer_id: customer.id, product_id: 1, uuid: SecureRandom.uuid)
 
-		redirect_to purchase
+      redirect_to purchase
 
 
-	rescue Stripe::CardError => e
-	  flash[:error] = e.message
-	  redirect_to charges_path
-	end
+      rescue Stripe::CardError => e
+        flash[:error] = e.message
+        redirect_to charges_path
+      end
 end
--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,6 +1,6 @@
 class PurchaseMailer < ActionMailer::Base
 	layout 'purchase_mailer'
-	default from: "mattsmittt@gmail.com"
+	default from: "Matt Smitt Payments <mattsmittt@gmail.com>"
 
 	def purchase_receipt purchase
 		@purchase = purchase
