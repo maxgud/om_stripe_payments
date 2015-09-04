70c864ea1cf59ae21e02a73d72a2c907795e3cc4
maxgud <maxgud@gmail.com>
2015-09-04 14:31:56 -0600

    Removed old home page landing page

--- app/controllers/pages_controller.rb
+++
@@ -1,9 +0,0 @@
-class PagesController < ApplicationController
-	def home
-		@product = Product.find_by_sku("GROHACK2")
-	end
-end
-
-# The first line means that this page will inherit all the properties of the ApplicationController page 
-
- 
